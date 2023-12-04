cf48e2d657aa77b7a18f09b2613b1da7ab3ec439
Dmytro <dmitry.yurin2020@gmail.com>
2023-12-04 14:56:58 +0200

    fix status bar styles for static content

--- src/components/AdminPage/StaticContent/StaticContent.tsx
+++ src/components/AdminPage/StaticContent/StaticContent.tsx
@@ -1,9 +1,14 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootStateType } from "@/redux/types";
-import { fetchStaticContent, updateStaticContent, generateWebpageData, getPending_changes_status } from "@/redux/slices/staticContent.slice";
+import {
+  fetchStaticContent,
+  updateStaticContent,
+  generateWebpageData,
+  getPending_changes_status,
+} from "@/redux/slices/staticContent.slice";
 import { updateStaticContentFn } from "@/components/AdminPage/types";
-import { AdminPageQuries } from '@/components/AdminPage/types';
+import { AdminPageQuries } from "@/components/AdminPage/types";
 import HeadlinesTable from "./Tables/HeadlinesTable/HeadlinesTable";
 import ContentTable from "./Tables/ContentTable/ContentTable";
 import ImagesTable from "./Tables/ImagesTable/ImagesTable";
@@ -14,25 +19,35 @@ import global from "@/styles/global.module.scss";
 import Button from "@/components/ui/Button/Button";
 import DeploymentStatusBar from "../DeploymentStatusBar/DeploymentStatusBar";
 
-interface StaticContentProps extends AdminPageQuries { }
+interface StaticContentProps extends AdminPageQuries {}
 
 const StaticContent: React.FC<StaticContentProps> = ({ lang }) => {
   const [activeTableType, setActiveTableType] = useState(
     localStorage.getItem("activeTab") || "Headlines"
   );
-  const staticContent = useSelector((state: RootStateType) => state.staticContent.staticContent)
-  const pending_changes = useSelector((state: RootStateType) => state.staticContent.pending_changes)
-  const fetchStaticContentStatus = useSelector((state: RootStateType) => state.staticContent.fetchStaticContentStatus)
-  const generateWebpageDataStatus = useSelector((state: RootStateType) => state.staticContent.generateWebpageDataStatus)
+  const staticContent = useSelector(
+    (state: RootStateType) => state.staticContent.staticContent
+  );
+  const pending_changes = useSelector(
+    (state: RootStateType) => state.staticContent.pending_changes
+  );
+  const fetchStaticContentStatus = useSelector(
+    (state: RootStateType) => state.staticContent.fetchStaticContentStatus
+  );
+  const generateWebpageDataStatus = useSelector(
+    (state: RootStateType) => state.staticContent.generateWebpageDataStatus
+  );
 
   const dispatch = useDispatch<AppDispatch>();
 
   useEffect(() => {
-    dispatch(fetchStaticContent())
-    dispatch(getPending_changes_status())
-  }, [])
+    dispatch(fetchStaticContent());
+    dispatch(getPending_changes_status());
+  }, []);
 
-  const currData = staticContent.find(item => item.language.toUpperCase() === lang?.toUpperCase())
+  const currData = staticContent.find(
+    (item) => item.language.toUpperCase() === lang?.toUpperCase()
+  );
 
   const onTableTypeClick = (tableName: string) => {
     setActiveTableType(tableName);
@@ -44,30 +59,54 @@ const StaticContent: React.FC<StaticContentProps> = ({ lang }) => {
       dispatch(
         updateStaticContent({
           id: currData.id,
-          data
+          data,
         })
       );
     }
-  }
+  };
 
   const onSave = () => {
-    dispatch(generateWebpageData())
-      .then((() => setUpdateMoment()))
-  }
-  const saveButtonValue = generateWebpageDataStatus === "loading" ? "Loading..." :
-    generateWebpageDataStatus === "error" ? "error" : "Save"
+    dispatch(generateWebpageData()).then(() => setUpdateMoment());
+  };
+  const saveButtonValue =
+    generateWebpageDataStatus === "loading"
+      ? "Loading..."
+      : generateWebpageDataStatus === "error"
+      ? "error"
+      : "Save";
 
   const setUpdateMoment = () => {
-    localStorage.setItem("updateMoment", (new Date()).toISOString())
-  }
+    localStorage.setItem("updateMoment", new Date().toISOString());
+  };
 
   const tables: {
     [key: string]: JSX.Element;
   } = {
-    Headlines: <HeadlinesTable lang={lang} activeTableType={activeTableType} onUpdate={onTextContentUpdate} data={currData?.heading_tags || {}} />,
-    Content: <ContentTable lang={lang} activeTableType={activeTableType} onUpdate={onTextContentUpdate} data={currData?.content || {}} />,
+    Headlines: (
+      <HeadlinesTable
+        lang={lang}
+        activeTableType={activeTableType}
+        onUpdate={onTextContentUpdate}
+        data={currData?.heading_tags || {}}
+      />
+    ),
+    Content: (
+      <ContentTable
+        lang={lang}
+        activeTableType={activeTableType}
+        onUpdate={onTextContentUpdate}
+        data={currData?.content || {}}
+      />
+    ),
     Images: <ImagesTable activeTableType={activeTableType} />,
-    Meta: <MetaTable lang={lang} activeTableType={activeTableType} onUpdate={onTextContentUpdate} data={currData?.meta_tags || {}} />,
+    Meta: (
+      <MetaTable
+        lang={lang}
+        activeTableType={activeTableType}
+        onUpdate={onTextContentUpdate}
+        data={currData?.meta_tags || {}}
+      />
+    ),
   };
 
   return (
@@ -78,12 +117,12 @@ const StaticContent: React.FC<StaticContentProps> = ({ lang }) => {
             activeTab={activeTableType}
             onTabClick={onTableTypeClick}
           />
-          {pending_changes &&
-            <Button onClick={() => onSave()}>
-              {saveButtonValue}
-            </Button>
-          }
-          <DeploymentStatusBar />
+          <div className={styles.headStatus}>
+            <DeploymentStatusBar />
+            {pending_changes && (
+              <Button onClick={() => onSave()}>{saveButtonValue}</Button>
+            )}
+          </div>
         </div>
         {tables[activeTableType]}
       </div>
@@ -92,4 +131,3 @@ const StaticContent: React.FC<StaticContentProps> = ({ lang }) => {
 };
 
 export default StaticContent;
-
