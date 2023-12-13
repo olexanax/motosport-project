38db0910070758c5136e14063cbc55c538cc4753
Oleksandr Sylko <sylkoalex@gmail.com>
2023-12-13 11:44:53 +0200

    metadata improvements

--- src/actions/get-new-by-slug.ts
+++ src/actions/get-new-by-slug.ts
@@ -1,24 +1,25 @@
 import { axiosInstance } from "@/lib/axios";
 
 export interface News {
-  id: number;
-  /** MMMM d yyyy format */
-  date: string;
-  title: string;
-  description: string;
-  image: string;
-  views: number;
-  slug: string;
-  language: "EN" | "UA";
-  meta_tags: string;
+    id: number;
+    /** MMMM d yyyy format */
+    date: string;
+    title: string;
+    description: string;
+    image: string;
+    views: number;
+    slug: string;
+    language: "EN" | "UA";
+    meta_title: string;
+    meta_description: string;
 }
 
 export const getNewsBySlug = async (slug: string) => {
-  try {
-    const { data } = await axiosInstance.get(`/news/${slug}/`);
+    try {
+        const { data } = await axiosInstance.get(`/news/${slug}/`);
 
-    return data as News;
-  } catch (error) {
-    return;
-  }
+        return data as News;
+    } catch (error) {
+        return;
+    }
 };
--- src/app/[lng]/(admin)/login/page.tsx
+++ src/app/[lng]/(admin)/login/page.tsx
@@ -1,84 +1,27 @@
-"use client";
-//types
 
-//libs
-import classNames from "classnames";
-import { useForm } from "react-hook-form";
-import { useEffect, useState } from "react";
-import { useRouter } from "next/navigation";
-//styles
-import global from "@/styles/global.module.scss";
-import styles from "./styles.module.scss";
-//redux
-import { AppDispatch, LoginFormInputs, RootStateType } from "@/redux/types";
-import { loginUser } from "@/redux/slices/login.slice";
-import { useDispatch, useSelector } from "react-redux";
+import LoginPageClientWrapper from "@/components/AdminPage/LoginPageClientWrapper/LoginPageClientWrapper";
+import { Metadata } from "next";
+//i18n
+import { I18PageProps } from "@/types/i18NextTypes";
+import { useTranslation } from "../../../i18n";
 
-export const fetchCache = "force-no-store";
-export const revalidate = 0;
-export const dynamic = "force-dynamic";
 
 const LoginPage = () => {
-  const dispatch = useDispatch<AppDispatch>();
-  const error = useSelector((state: RootStateType) => state.login.isLoginError);
-  const isLogining = useSelector(
-    (state: RootStateType) => state.login.isLoginLoading
-  );
-  const router = useRouter();
-  useEffect(() => {
-    if (!isLogining && localStorage.getItem("accessMotosport") != null) {
-      router.push("/admin");
-    }
-    // eslint-disable-next-line
-  }, [isLogining]);
-
-  const {
-    register,
-    handleSubmit,
-    formState: { isValid },
-    reset,
-  } = useForm<LoginFormInputs>();
-
-  const submitForm = (data: LoginFormInputs) => {
-    dispatch(loginUser(data));
-  };
-
   return (
-    <div id="about-us" className={styles.container}>
-      <div className={styles.content}>
-        <h1 className={classNames(styles.title, global.pageTitle)}>Welcome!</h1>
-        <form className={styles.form} onSubmit={handleSubmit(submitForm)}>
-          <div className={styles.inputBlock}>
-            <p className={styles.label}>Log In</p>
-            <input
-              type="text"
-              {...register("username", { required: true })}
-              className={styles.input}
-            />
-          </div>
-          <div className={styles.inputBlock}>
-            <p className={styles.label}>Password</p>
-            <input
-              type="password"
-              {...register("password", { required: true })}
-              className={styles.input}
-            />
-          </div>
-          <input
-            disabled={isLogining || !isValid}
-            type="submit"
-            value={`${isLogining ? "Loading..." : "Sign In"}`}
-            className={classNames(styles.btn, global.primaryButton)}
-          />
-          {error && (
-            <div className={styles.error}>
-              Something went wrong, please try again !
-            </div>
-          )}
-        </form>
-      </div>
-    </div>
+    <LoginPageClientWrapper />
   );
 };
+
+export async function generateMetadata({
+  params: { lng },
+}: I18PageProps): Promise<Metadata> {
+  const { t } = await useTranslation(lng, "translation");
+
+  return {
+    title: t("meta_tags.meta_title"),
+    description: t("meta_tags.meta_description"),
+    robots: "noindex"
+  };
+}
 
 export default LoginPage;
